1561cbf7fa652369fdbeaedf4f548edb770f273d
DESKTOP-9LQJFVE\JulioHenrique <juliohenrique_lima@hotmail.com>
2020-01-21 23:27:01 -0300

    Adicionei socket.io para novos devs cadastrados

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
 
@@ -35,7 +36,12 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
-
+            //Filtrar as conexões que estão há no máximo 10 km de distânci e que o novo dev tenha pelo menos uma das tecnologias filtradas.
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            );
+            sendMessage(sendSocketMessageTo, 'new-dev', dev)
         }
         return res.json(dev)
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
 const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb://127.0.0.1/omnistack10', {
     useNewUrlParser: true,
@@ -14,7 +20,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333);
+server.listen(3333);
 
 /**
  *
